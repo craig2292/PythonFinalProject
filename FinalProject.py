537c5d33c3b9919376bc12e41000f9f9c6967c57
craig2292 <craig2292@gmail.com>
2015-12-07 13:23:46 -0500

    More Updates

--- FinalProject.py
+++ FinalProject.py
@@ -5,7 +5,7 @@ from arcpy.sa import *
 
 
 ##Set up workspace
-env.workspace = "F:\Fall2015\Python\PythonProject\Data"
+env.workspace = "E:\Fall2015\Python\PythonProject\Data"
 env.overwriteOutput = True
 
 
@@ -20,14 +20,14 @@ arcpy.CheckOutExtension("Spatial")
 outExtractByMask = ExtractByMask(inRaster, inMaskData)
 
 # Save the output 
-outExtractByMask.save("F:\Fall2015\Python\PythonProject\Data\Outputs\ClarkeCo.tif")
+outExtractByMask.save("E:\Fall2015\Python\PythonProject\Data\Outputs\ClarkeCo.tif")
 
 
 
 
 
 #IsoCluster Tool
-inRaster = "F:\Fall2015\Python\PythonProject\Data\Outputs\ClarkeCo.tif"
+inRaster = "E:\Fall2015\Python\PythonProject\Data\Outputs\ClarkeCo.tif"
 classes = 6
 minMembers = 20
 sampInterval = 10
@@ -39,22 +39,22 @@ arcpy.CheckOutExtension("Spatial")
 outUnsupervised = IsoClusterUnsupervisedClassification(inRaster, classes, minMembers, sampInterval)
 
 #Save the Output
-outUnsupervised.save("F:\Fall2015\Python\PythonProject\Data\Outputs\AOI_IsoClust.tif")
+outUnsupervised.save("E:\Fall2015\Python\PythonProject\Data\Outputs\AOI_IsoClust.tif")
 
 
 
 
 
 #Raster to Polygon Tool
-inRaster = "F:\Fall2015\Python\PythonProject\Data\Outputs\AOI_IsoClust.tif"
-outPolygons = "F:\Fall2015\Python\PythonProject\Data\Outputs\Polygons.shp"
+inRaster = "E:\Fall2015\Python\PythonProject\Data\Outputs\AOI_IsoClust.tif"
+outPolygons = "E:\Fall2015\Python\PythonProject\Data\Outputs\Polygons.shp"
 field = "VALUE"
 
 #Execute Raster To Polygon
 arcpy.RasterToPolygon_conversion(inRaster, outPolygons, "NO_SIMPLIFY", field)
 
 #Extract by Attributes Tool
-inRaster = "F:\Fall2015\Python\PythonProject\Data\Outputs\AOI_IsoClust.tif"
+inRaster = "E:\Fall2015\Python\PythonProject\Data\Outputs\AOI_IsoClust.tif"
 inSQLClause = "VALUE = 6"
 
 #Check out the ArcGIS Spatial Analyst Extension License
@@ -64,13 +64,13 @@ arcpy.CheckOutExtension("Spatial")
 attExtract = ExtractByAttributes(inRaster, inSQLClause)
 
 #Save the Output
-attExtract.save("F:\Fall2015\Python\PythonProject\Data\Outputs\UrbanExtract.tif")
+attExtract.save("E:\Fall2015\Python\PythonProject\Data\Outputs\UrbanExtract.tif")
 
 
 
-#Raster to Polygon tool for water extract
-inRaster = "F:\Fall2015\Python\PythonProject\Data\Outputs\UrbanExtract.tif"
-outPolygons = "F:\Fall2015\Python\PythonProject\Data\Outputs\UrbanPolygons.shp"
+#Raster to Polygon tool for urban extract
+inRaster = "E:\Fall2015\Python\PythonProject\Data\Outputs\UrbanExtract.tif"
+outPolygons = "E:\Fall2015\Python\PythonProject\Data\Outputs\UrbanPolygons.shp"
 field = "VALUE"
 
 #Execute Raster to Polygon
@@ -79,7 +79,7 @@ arcpy.RasterToPolygon_conversion(inRaster, outPolygons, "NO_SIMPLIFY", field)
 
 
 #Create new field
-inFeatures = "F:\Fall2015\Python\PythonProject\Data\Outputs\UrbanPolygons.shp"
+inFeatures = "E:\Fall2015\Python\PythonProject\Data\Outputs\UrbanPolygons.shp"
 fieldName1 = "Area"
 fieldPrecision = 15
 fieldAlias = "refcode"
@@ -92,14 +92,9 @@ arcpy.AddField_management(inFeatures, fieldName1, "FLOAT", fieldPrecision, "", "
 #Calculate Geometry of Area field with Calculate Field tool
 arcpy.CalculateField_management(inFeatures, fieldName1, "!SHAPE.area!", "PYTHON_9.3")
 
-#Select by Attribute Area greater than 1000 square meters
-arcpy.MakeFeatureLayer_management("F:\Fall2015\Python\PythonProject\Data\Outputs\UrbanPolygons.shp", "Outlyr")
+#Select by Attribute Area greater than 0 square meters
+arcpy.MakeFeatureLayer_management("E:\Fall2015\Python\PythonProject\Data\Outputs\UrbanPolygons.shp", "Outlyr")
 
-arcpy.SelectLayerByLocation_management ("Outlyr", "intersect", "F:\Fall2015\Python\PythonProject\Data\Outputs\Polygons.shp", 0, "new_selection")
+arcpy.SelectLayerByLocation_management ("Outlyr", "intersect", "E:\Fall2015\Python\PythonProject\Data\Outputs\Polygons.shp", 0, "new_selection")
 arcpy.SelectLayerByAttribute_management("Outlyr", "SUBSET_SELECTION", ' "AREA" > 0 ')
 arcpy.CopyFeatures_management("Outlyr", "Urban")
-
-
-
-
-
